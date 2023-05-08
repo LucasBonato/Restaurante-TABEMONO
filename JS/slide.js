7cb0b3a9a4b32a1e4065c5e50ce9ed12bd56bfc0
Lucas Bonato <lucas.perez.bonato@gmail.com>
2023-05-08 02:36:28 -0300

    Using and implementing the slider in the index

--- JS/slide.js
+++ JS/slide.js
@@ -1,32 +1,29 @@
 
 let eleImg = document.getElementById("imgSlide");
-let inputs = document.getElementsByClassName("teste");
+let inputs = document.getElementsByClassName("inpuSlider");
 let inpuLengh = inputs.length;
 
-let img1 = "../img/back.png";
-let img2 = "../img/comida-back-final.jpeg";
-let img3 = "../img/mesa-back-final.jpg";
+let imgs = ["../img/back.png", "../img/comida-back-final.jpeg", "../img/mesa-back-final.jpg"]
 
-let count = 1;
+let count = 0;
 let time = 5000;
 
 let timer = setInterval("Show()", time);
 
 const Show = () => {
     
-    let inpuRadio = document.getElementById("v" + count);
+    let inpuRadio = document.getElementById("v" + (count + 1));
 
-    eleImg.src = eval("img" + count);
+    eleImg.src = imgs[count];
     inpuRadio.checked = true;
 
-    count < inpuLengh ? count++ : count = 1;
+    count < (inpuLengh - 1) ? count++ : count = 0;
 }
 
 for (let i = 0; i < inpuLengh; i++){
     inputs[i].addEventListener("click", function Change() {
         clearInterval(timer);
-        count = i + 1;
-        eleImg.src = eval("img" + count);
+        eleImg.src = imgs[i];
         timer = setInterval("Show()", time);
     });
 }
