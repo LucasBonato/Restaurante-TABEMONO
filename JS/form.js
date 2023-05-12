149cbf229245ed4559bfe9d66edb65aca27b9bb0
Lucas Bonato <lucas.perez.bonato@gmail.com>
2023-05-11 23:22:16 -0300

    Concertando o forms
    Usando só CSS para reformular os labels dentro do input

--- JS/form.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementsByClassName("teste")
-const inputLista = input.length;
-
-const Alterar = event => {
-    for(let i = 0; i <= inputLista; i++) {
-        input[i].classList.toggle("temValor", event.target.value);
-    }
-}
-
